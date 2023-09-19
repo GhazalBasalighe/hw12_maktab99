d64e01afb286a59e9ad0c6f172fe1cb629192451
Ghazal Basalighe <ghazalbasaligheh@gmail.com>
2023-09-20 02:52:09 +0330

    [feat] Add delete functionality

--- Front End/scripts/todos.js
+++ Front End/scripts/todos.js
@@ -1,8 +1,10 @@
 const checkButton = document.querySelectorAll("#checked-btn");
+const gridContainer = document.querySelector(".grid-container");
 const gridItem = document.querySelector(".grid-item");
 const editIcon = document.querySelector(".edit-icon");
-const deleteIcon = document.querySelector(".delete-icon");
+const deleteIcon = document.querySelectorAll(".delete-icon");
 
+//------------RENDERING TASKS AND GET REQUEST-------------
 async function renderTasks() {
   try {
     let response = await fetch("http://localhost:3000/tasks");
@@ -12,20 +14,21 @@ async function renderTasks() {
     console.log(error.massage);
   }
 }
-async function makeGridItems() {
-  const gridContainer = document.querySelector(".grid-container");
+
+//------------GENERATE GRID ITEMS-------------
+async function generateTasks() {
   const tasks = await renderTasks();
   tasks.forEach((task) => {
-    const content = `<div class="grid-item">
+    const content = `<div class="grid-item" data-task-id="${task.id}">
                         <div class="first-line">
                            <div class="info">
-                              <div class="blank-circle" id="checked-btn" data-taskId="1" data-status="unDone"></div>
+                            <input type="checkbox" name="checkbox" class="blank-circle" id="checked-btn">
                               <h6>${task.title}</h6>
                               <p>${task.dueDate}</p>
                            </div>
                            <div class="task-controls">
                               <div class="edit-icon"><img src="../images/icon edit.svg" alt="edit icon"></div>
-                              <div class="delete-icon"><img src="../images/icon x.png" alt="delete icon"></div>
+                              <div><img src="../images/icon delete.svg" alt="delete icon" class="delete-icon"></div>
                            </div>
                         </div>
                         <p>${task.description}</p>
@@ -34,7 +37,32 @@ async function makeGridItems() {
     gridContainer.insertAdjacentHTML("beforeend", content);
   });
 }
-makeGridItems();
+generateTasks();
+
+//------------DELETE TASK FROM DATABASE-------------
+// deleteIcon.forEach((element) => {
+//   element.addEventListener("click", async () => {
+//     const gridItem = element.closest(".grid-item");
+//     const taskId = gridItem.dataset.taskId; // Get the task ID from the data attribute
+//     // const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
+//     //   method: "DELETE",
+//     // });
+//     // let response2 = await response.json();
+//     console.log("hellooooo");
+//   });
+// });
+gridContainer.addEventListener("click", async (event) => {
+  if (event.target.classList.contains("delete-icon")) {
+    const target = event.target;
+    const gridItem = target.closest(".grid-item");
+    const taskId = gridItem.dataset.taskId; // Get the task ID from the data attribute
+    const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
+      method: "DELETE",
+    });
+    target.remove();
+  }
+});
+
 // checkButton.forEach((item) => {
 //   item.addEventListener("click", async function () {
 //     const taskId = this.getAttribute("data-taskId");
