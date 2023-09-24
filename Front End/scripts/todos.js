73305079ee98f28d2bcba62a63352a2174a7bcea
Ghazal Basalighe <ghazalbasaligheh@gmail.com>
2023-09-24 04:02:31 +0330

    [feat] Add pagination for todos.html

--- Front End/scripts/todos.js
+++ Front End/scripts/todos.js
@@ -3,10 +3,15 @@ import { getJSON, updateJSON } from "./requests.js";
 const gridContainer = document.querySelector(".grid-container");
 
 //------------RENDERING TASKS------------
-getJSON();
+generateTasks();
 // ------------GENERATE GRID ITEMS-------------
-(async function generateTasks() {
-  const tasks = await getJSON();
+async function generateTasks(queryParam = "?_page=1&_limit=12") {
+  const tasks = await getJSON(queryParam);
+  if (queryParam) {
+    // gridContainer.innerHTML = "";
+    gridContainer.textContent = "";
+    //According to MDN this will be faster than innerHTML as browsers won't invoke their HTML parsers and will instead immediately replace all children of the element with a single #text node.
+  }
   tasks.forEach((task) => {
     const content = `<div class="grid-item" data-task-id="${task.id}">
                         <div class="first-line">
@@ -25,7 +30,7 @@ getJSON();
 `;
     gridContainer.insertAdjacentHTML("beforeend", content);
   });
-})();
+}
 
 //------------GENERATE MODALS FOR TASKS-------------
 async function generateModal(task) {
@@ -128,5 +133,52 @@ document.addEventListener("click", (event) => {
       modal.remove();
       updateJSON("DELETE", taskId);
     }
+  }
+});
+
+//------------PAGINATION------------
+const footer = document.querySelector(".bottom-gray");
+
+const taskPerPage = 12;
+const tasksTotalCount = (await getJSON()).length;
+const pageCount = Math.ceil(tasksTotalCount / taskPerPage);
+let currentPage = 1;
+
+//dynamic ui creation for page numbers
+for (let i = 1; i <= pageCount; i++) {
+  const pageNum = document.createElement("div");
+  pageNum.textContent = i;
+  pageNum.classList.add("page-number");
+  pageNum.id = i;
+  footer.appendChild(pageNum);
+}
+
+//------------INSERT ARROWS FOR PAGINATION-------------
+
+function insertArrowSvg(elemName, innerHtml, placeTo) {
+  elemName.innerHTML = `${innerHtml}`;
+  elemName.style.cursor = "pointer";
+  footer.insertAdjacentElement(`${placeTo}`, elemName);
+}
+//left arrow at the very beginning
+const prevPage = document.createElement("span");
+const prevpageSvg = ` <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g transform="translate(24 0) scale(-1 1)"><g fill="none" fill-rule="evenodd"><path d="M24 0v24H0V0h24ZM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01l-.184-.092Z"/><path fill="#1ab8db" d="M16.06 10.94a1.5 1.5 0 0 1 0 2.12l-5.656 5.658a1.5 1.5 0 1 1-2.121-2.122L12.879 12L8.283 7.404a1.5 1.5 0 0 1 2.12-2.122l5.658 5.657Z"/></g></g></svg>`;
+insertArrowSvg(prevPage, prevpageSvg, "afterbegin");
+//right arrow at the very end
+const nextPage = document.createElement("span");
+const nextPageSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path d="M24 0v24H0V0h24ZM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01l-.184-.092Z"/><path fill="#1ab8db" d="M16.06 10.94a1.5 1.5 0 0 1 0 2.12l-5.656 5.658a1.5 1.5 0 1 1-2.121-2.122L12.879 12L8.283 7.404a1.5 1.5 0 0 1 2.12-2.122l5.658 5.657Z"/></g></svg>`;
+insertArrowSvg(nextPage, nextPageSvg, "beforeend");
+
+//search params and query
+footer.addEventListener("click", async (e) => {
+  if (e.target.classList.contains("page-number")) {
+    const pageId = e.target.id;
+    currentPage = pageId;
+    const queryParam = `?_page=${pageId}&_limit=${taskPerPage}`;
+    generateTasks(queryParam);
+    const pageAddress = new URL(window.location.href);
+    pageAddress.searchParams.set("_page", pageId);
+    pageAddress.searchParams.set("_limit", taskPerPage);
+    window.history.replaceState({}, "", pageAddress);
   }
 });
